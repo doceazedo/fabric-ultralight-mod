451df928cc96ed5110fe47ef7ce28210055198c4
Lucas <eu@lucasmellof.com>
2023-08-23 22:23:06 -0300

    Fixing startup

--- src/client/java/com/doceazedo/mixin/client/MixinWindow.java
+++ src/client/java/com/doceazedo/mixin/client/MixinWindow.java
@@ -4,8 +4,11 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 /*
@@ -13,12 +16,24 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  */
 @Mixin(Window.class)
 public class MixinWindow {
+	@Shadow private int framebufferWidth;
+
+	@Shadow private int framebufferHeight;
+
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwDefaultWindowHints()V"), remap = false)
 	public void fixRetina() {
 		GLFW.glfwDefaultWindowHints();
 
 		if (MinecraftClient.IS_SYSTEM_MAC) {
 			GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
+		}
+	}
+
+	@Inject(at = @At(value = "RETURN"), method = "updateFramebufferSize")
+	private void afterUpdateFrameBufferSize(CallbackInfo ci) {
+		if (MinecraftClient.IS_SYSTEM_MAC ) {
+			framebufferWidth /= 2;
+			framebufferHeight /= 2;
 		}
 	}
 }
